175d9837072237bc8e68f6437944757d9f2c8735
Stephanie Mitchell <stephsorandom@gmail.com>
2021-04-20 17:05:03 -0400

    adding subscription email

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,33 @@
-import "./App.css";
-import NavBar from "./Components/NavBar";
+import './App.css'
+import NavBar from './Components/NavBar'
+import Footer from './Components/Footer'
 
-import SignUp from "./Components/SignUp";
-import Login from "./Components/Login";
-import Profile from "./Components/Profile";
-import TheContext from "./TheContext";
+import SignUp from './Components/SignUp'
+import Login from './Components/Login'
+import Profile from './Components/Profile'
+import TheContext from './TheContext'
 
-import LandingPage from "./Components/LandingPage";
-import { Route, Switch, useHistory } from "react-router-dom";
-import { useState } from "react";
+import LandingPage from './Components/LandingPage'
+import { Route, Switch, useHistory } from 'react-router-dom'
+import { useState } from 'react';
 
 function App() {
   let [user, setUser] = useState(null);
   const history = useHistory();
   return (
-    <div className="App">
+    <div className='App'>
       
         <TheContext.Provider value={{ history, user, setUser }}>
           <NavBar />
           <LandingPage />
           <Switch>
-            <Route exact path="/signup" render={(props) => <SignUp />} />
-            <Route exact path="/login" render={(props) => <Login {...props} />}
+            <Route exact path='/signup' render={(props) => <SignUp />} />
+            <Route exact path='/login' render={(props) => <Login {...props} />}
             />
-            <Route exact path="/profile" render={(props) => <Profile {...props} />}
+            <Route exact path='/profile' render={(props) => <Profile {...props} />}
             />
           </Switch>
+          <Footer />
         </TheContext.Provider>
      
     </div>
