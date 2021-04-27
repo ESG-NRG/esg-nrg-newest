230ec655b6f2153baf537cd65e8d33fcecf6ccf3
Greg King <vrodgreg@gmail.com>
2021-04-27 19:29:19 -0400

    Fixed the mobile version mostly commented in nav and footer

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -1,7 +1,8 @@
-import { useState, useContext } from 'react';
+import { useState, useContext } from "react";
 import { Link } from "react-router-dom";
-import actions from '../api/index';
-import TheContext from '../TheContext';
+import useScreenSize from "./useScreenSize";
+import actions from "../api/index";
+import TheContext from "../TheContext";
 import loginPic from "../images/loginPic.png";
 import largeEicon from "../images/largeEicon.svg";
 import xIcon from "../images/xIcon.png";
@@ -12,18 +13,22 @@ import "../signUpIn.css";
 export default function Login() {
   const { history, user, setUser } = useContext(TheContext);
   const [userData, setUserData] = useState({
-    email: '',
-    password: '',
+    email: "",
+    password: "",
   });
+
+  const screenSize = useScreenSize();
+  let phone = false;
+  screenSize === "xs" || screenSize === "sm" ? (phone = true) : (phone = false);
 
   const signupUser = (e) => {
     e.preventDefault();
-    console.log("here we are at signupUser function")
+    console.log("here we are at signupUser function");
     actions
       .logIn(userData)
       .then((user) => {
         setUser({ ...user?.data }); //user info --> profile.js
-        history.push('/profile');
+        history.push("/profile");
       })
       .catch((error) => {
         const { data } = error.response;
@@ -31,18 +36,21 @@ export default function Login() {
       });
   };
   function handleChange(event) {
-    setUserData({...userData, [event.target.name]: event.target.value,});
+    setUserData({ ...userData, [event.target.name]: event.target.value });
   }
 
   function passwordsView(e) {
-    console.log("vewPwType before ternary", viewPwType)
-      viewPwType[e] !== "password" ? viewPwType[e] = "password" : viewPwType[e] = "text"
+    console.log("vewPwType before ternary", viewPwType);
+    viewPwType[e] !== "password"
+      ? (viewPwType[e] = "password")
+      : (viewPwType[e] = "text");
   }
 
-  let viewPwType=["password", "password"]
+  let viewPwType = ["password", "password"];
 
   return (
-    <section class="flex flex-row ...">
+    <section class={phone ? "nothing" : "flex flex-row ..."}>
+      {screenSize !== "xs" && screenSize !== "sm" ? (
         <div class="relative z-0 justify-items-center ...">
           <img src={loginPic} alt="Tanker on Water" width="317"></img>
           <img
@@ -56,45 +64,71 @@ export default function Login() {
             neque, volutpat ac.
           </p>
         </div>
+      ) : (
+        <div>
+          {/* <img
+          class="largeEMobile"
+          src={largeEicon}
+          alt="Large E Icon Logo"
+          width="58.8"
+        ></img> */}
+        </div>
+      )}
 
-        <div class="rightDiv">
-          <Link class="x" to="/">
-            <img src={xIcon} alt="X to cancel" width="20"></img>
+      <div class={phone ? "rightDivMobile" : "rightDiv"}>
+      {phone ? (
+              <div>
+                <img
+                  class="largeEMobile"
+                  src={largeEicon}
+                  alt="Large E Icon Logo"
+                  width="58.8"
+                ></img>
+              </div>
+            ) : (
+              <div></div>
+            )}
+        <Link class={phone ? "xMobile" : "x"} to="/">
+          <img src={xIcon} alt="X to cancel" width="20"></img>
+        </Link>
+        <h1 class={phone ? "welcomeMobile" : "welcome"}>Welcome Back!</h1>
+        <div
+          class={
+            phone
+              ? "flex flex-row alreadyDivMobile ..."
+              : "flex flex-row alreadyDiv ..."
+          }
+        >
+          <p class="already">Don't have an account? &nbsp;&nbsp;</p>
+          <Link class="signIn" to="/SignUp">
+            <p> Sign Up</p>
           </Link>
-          <h1 class="welcome">Welcome Back!</h1>
-          <div class="flex flex-row alreadyDiv ...">
-            <p class="already">Don't have an account? &nbsp;&nbsp;</p>
-            <Link class="signIn" to="/SignUp">
-              <p> Sign Up</p>
-            </Link>
-          </div>
-          <form class="form" onSubmit={signupUser}>
-            <br></br>
-            <p class="formName">Name</p>
+        </div>
+        <form class={phone ? "formMobile" : "form"} onSubmit={signupUser}>
+          <br></br>
+          <p class="formName">Name</p>
+          <input
+            // className="hover:border-blue-500"
+            name="name"
+            type="string"
+            onChange={handleChange}
+            placeholder=""
+            value={userData.name}
+          />{" "}
+          <br />
+          <br></br>
+          <p class="formName">Password</p>
+          <div class="passwordContainer">
             <input
-              // className="hover:border-blue-500"
-              name="name"
-              type="string"
+              className="inputPW"
+              name="password"
+              id="firstPassword"
+              type={viewPwType[1]}
               onChange={handleChange}
               placeholder=""
-              value={userData.name}
-            />{" "}
-            <br />
-            <br></br>
-           
-       
-            <p class="formName">Password</p>
-            <div class="passwordContainer">
-              <input
-                className="inputPW"
-                name="password"
-                id="firstPassword"
-                type={viewPwType[1]}
-                onChange={handleChange}
-                placeholder=""
-                value={userData.password}
-              />
-              {/* <input
+              value={userData.password}
+            />
+            {/* <input
                 type="image"
                 name="passwordView1"
                 id="passwordView"
@@ -102,36 +136,30 @@ export default function Login() {
                 src={pwView}
                 alt="View Password"
               ></input> */}
-              <br></br>
-              <br></br>
-               <p class="forgot">
-            Forgot Password?{" "}
-          </p>
-              <button type="image" src={pwView} id="passwordView" onclick={passwordsView(0)}></button>
-              {/* <img src={pwView} id="passwordView" onclick={passwordsView(0)} alt="view password"></img> */}
-              
-              <br />
-              <br></br>
-            </div>
-           
-              
-            
-              <button class="createBtn" type="submit">
-                Sign In
-              </button>
-            
-            {/* <button class="createBtn" type="submit">
+            <br></br>
+            <br></br>
+            <p class="forgot">Forgot Password? </p>
+            <button
+              type="image"
+              src={pwView}
+              id="passwordView"
+              onclick={passwordsView(0)}
+            ></button>
+            {/* <img src={pwView} id="passwordView" onclick={passwordsView(0)} alt="view password"></img> */}
+
+            <br />
+            <br></br>
+          </div>
+          <button class="createBtn" type="submit">
+            Sign In
+          </button>
+          {/* <button class="createBtn" type="submit">
               Create your account
             </button> */}
-          </form>
-          <br></br>
-          <p class="terms">
-            By creating an account, you agree to our Privacy Policy and our
-            Terms and Conditions.{" "}
-          </p>
-        </div>
-      </section>
-
+        </form>
+        <br></br>
+      </div>
+    </section>
 
     // <div>
     //   <h1>Login</h1>
--- client/src/Components/SignUp.jsx
+++ client/src/Components/SignUp.jsx
@@ -17,8 +17,8 @@ export default function Signup() {
     password: "",
   });
   const screenSize = useScreenSize();
-  let phone = false
-  screenSize === "xs" || screenSize === "sm" ? ( phone = true ) : phone = false
+  let phone = false;
+  screenSize === "xs" || screenSize === "sm" ? (phone = true) : (phone = false);
 
   const signupUser = (e) => {
     e.preventDefault();
@@ -59,7 +59,7 @@ export default function Signup() {
 
   return (
     <>
-      <section class={phone ? "nothing" : "flex flex-row ..." }>
+      <section class={phone ? "nothing" : "flex flex-row ..."}>
         {screenSize !== "xs" && screenSize !== "sm" ? (
           <div class="relative z-0 justify-items-center ...">
             <img src={loginPic} alt="Tanker on Water" width="317"></img>
@@ -85,24 +85,42 @@ export default function Signup() {
           </div>
         )}
 
-        <div class={phone ?"rightDivMobile" : "rightDiv" }>
-          {phone ? (<div><img
-              class="largeEMobile"
-              src={largeEicon}
-              alt="Large E Icon Logo"
-              width="58.8"
-            ></img></div>) : (<div></div>) }
-          <Link class="x" to="/">
-            <img src={xIcon} alt="X to cancel" width="20"></img>
-          </Link>
-          <h1 class="welcome">Welcome to ESG-NRG!</h1>
-          <div class="flex flex-row alreadyDiv ...">
+        <div class={phone ? "rightDivMobile" : "rightDiv"}>
+          <div>
+            {" "}
+            {phone ? (
+              <div>
+                <img
+                  class="largeEMobile"
+                  src={largeEicon}
+                  alt="Large E Icon Logo"
+                  width="58.8"
+                ></img>
+              </div>
+            ) : (
+              <div></div>
+            )}
+            <Link class={phone ? "xMobile" : "x"} to="/">
+              <img src={xIcon} alt="X to cancel" width="20"></img>
+            </Link>
+          </div>
+
+          <h1 class={phone ? "welcomeMobile" : "welcome"}>
+            Welcome to ESG-NRG!
+          </h1>
+          <div
+            class={
+              phone
+                ? "flex flex-row alreadyDivMobile ..."
+                : "flex flex-row alreadyDiv ..."
+            }
+          >
             <p class="already">Already have an account? &nbsp;&nbsp;</p>
             <Link class="signIn" to="/login">
               <p> Sign In</p>
             </Link>
           </div>
-          <form class="form" onSubmit={signupUser}>
+          <form class={phone ? "formMobile" : "form"} onSubmit={signupUser}>
             <br></br>
             <p class="formName">Name</p>
             <input
@@ -224,7 +242,7 @@ export default function Signup() {
             </button> */}
           </form>
           <br></br>
-          <p class="terms">
+          <p class={phone ? "termsMobile" : "terms"}>
             By creating an account, you agree to our Privacy Policy and our
             Terms and Conditions.{" "}
           </p>
