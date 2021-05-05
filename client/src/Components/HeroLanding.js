b253b8988e127ad5756d3207435d5fd9cf1fdedf
Stephanie Mitchell <stephsorandom@gmail.com>
2021-05-05 16:26:59 -0400

    hero desktop letters fixed

--- client/src/Components/HeroLanding.js
+++ client/src/Components/HeroLanding.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import useScreenSize from "./useScreenSize";
 import heropic from "../images/heropic.png";
+import hero from '../images/Hero.png';
 import heroM from "../images/heroM.png";
 
 function HeroLanding(props) {
@@ -17,7 +18,11 @@ function HeroLanding(props) {
         </div>
       ) : (
         <div>
-          <div className="bg-local ">
+        <div className="bg-local ">
+            {/* make img clickable with a href tag once button 'get started'  */}
+            <img src={hero} alt="hero pic" />
+          </div>
+          {/* <div className="bg-local ">
             <img className="heroz" src={heropic} alt="hero pic" />
           </div>
           <div className="centered">
@@ -34,7 +39,7 @@ function HeroLanding(props) {
             <br />
             <br />
             <button className="button">GET STARTED</button>
-          </div>
+          </div> */}
         </div>
       )}
     </>
