d9e52c5e3edbd1a95ada045ee8d5b2471d1d8632
Stephanie Mitchell <stephsorandom@gmail.com>
2021-05-06 17:24:24 -0400

    halfway done

--- client/src/Components/NavBar.js
+++ client/src/Components/NavBar.js
@@ -30,7 +30,7 @@ function NavBar(props) {
       {screenSize === "xs" || screenSize === "sm" ? (
         //hamburger
 
-        <Menu right >
+        <Menu right>
         <a id="home" className="menu-item" href="/">Home</a>
         <a id="about" className="menu-item" href="/about">About</a>
         <a id="contact" className="menu-item" href="/contact">Contact</a>
